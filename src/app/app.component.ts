2306af9f6b247a8a5b3bc90f502fdf12f4ab8ddf
brandenvs <92578986+brandenvs@users.noreply.github.com>
2025-04-06 17:22:27 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { TasksComponent } from "./tasks/tasks.component";
+import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS } from './dummy-users';
+import { NgFor, NgIf } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, TasksComponent, UserComponent, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'task-manager';
+  title = 'task-manager'
+
+  users = DUMMY_USERS;
+  selectedUserId?: string;
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId);
+  }
+
+  onSelectedUser(userId: string) {
+    this.selectedUserId = userId;
+  }
 }
